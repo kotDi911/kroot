701d045f64818fb413335c7af6d26838bcedc4a7
= <=>
2024-02-06 20:50:51 +0200

    v1

--- src/component/store/projects.js
+++ src/component/store/projects.js
@@ -2,6 +2,7 @@ import {create} from 'zustand'
 
 const URL = "https://github.com/kotDi911/kroot/blob/main/public/images/projects"
 const open = "?raw=true"
+
 export const useCards = create(() => ({
     projects: [
         {
@@ -26,7 +27,122 @@ export const useCards = create(() => ({
                 {img: ""},
                 {img: ""},
             ]
-        }
+        },
+        {
+            id: 2,
+            name: "",
+            img: `${URL}2.gif${open}`,
+            details: [
+                {
+                    img: "",
+                    title: "",
+                    description: "",
+                    options: []
+                }
+            ],
+            galleryTitle: "Gallery",
+            galleryDescription: "Gallery",
+            galleryImg: [
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+            ]
+        },
+        {
+            id: 3,
+            name: "",
+            img: `${URL}/3.gif${open}`,
+            details: [
+                {
+                    img: "",
+                    title: "",
+                    description: "",
+                    options: []
+                }
+            ],
+            galleryTitle: "Gallery",
+            galleryDescription: "Gallery",
+            galleryImg: [
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+            ]
+        },
+        {
+            id: 4,
+            name: "",
+            img: `${URL}/4.gif${open}`,
+            details: [
+                {
+                    img: "",
+                    title: "",
+                    description: "",
+                    options: []
+                }
+            ],
+            galleryTitle: "Gallery",
+            galleryDescription: "Gallery",
+            galleryImg: [
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+            ]
+        },
+        {
+            id: 5,
+            name: "",
+            img: `${URL}/5.gif${open}`,
+            details: [
+                {
+                    img: "",
+                    title: "",
+                    description: "",
+                    options: []
+                }
+            ],
+            galleryTitle: "Gallery",
+            galleryDescription: "Gallery",
+            galleryImg: [
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+            ]
+        },
+        {
+            id: 6,
+            name: "",
+            img: `${URL}/6.gif${open}`,
+            details: [
+                {
+                    img: "",
+                    title: "",
+                    description: "",
+                    options: []
+                }
+            ],
+            galleryTitle: "Gallery",
+            galleryDescription: "Gallery",
+            galleryImg: [
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+                {img: ""},
+            ]
+        },
     ],
 
 }));
