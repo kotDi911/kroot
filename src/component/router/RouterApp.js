d94f553441a955a2e383c42da9b55c100882f55b
= <=>
2024-02-09 00:08:04 +0200

    upd v2

--- src/component/router/RouterApp.js
+++ src/component/router/RouterApp.js
@@ -1,7 +1,7 @@
 import {Navigate, Outlet, Routes} from "react-router";
 import {NavLink, Route, useLocation} from "react-router-dom";
 import Home from "../Pages/Home";
-import logo from "../../assets/ico/Logo.png"
+import logo from "../../assets/ico/Logo.svg"
 import About from "../Pages/About";
 import Services from "../Pages/Services";
 import Projects from "../Pages/Projects";
